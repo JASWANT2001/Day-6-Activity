3176100bff2ad6f866a66414f436b272651d56d4
Jaswant K <113849693+JASWANT2001@users.noreply.github.com>
2023-09-28 18:29:37 +0530

    Add files via upload

---
+++ Day - 6.js
@@ -0,0 +1,9 @@
+class movie {
+    constructor(title,studio,rating){
+        this.title = title
+        this.studio = studio
+        this.rating = rating
+    }
+}
+var list = new movie("Leo","7Screen","PG-14")
+console.log(list)
